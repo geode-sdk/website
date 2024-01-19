e1a9e6e48935f56001c5a7548ee9018576b53c1f
matcool <26722564+matcool@users.noreply.github.com>
2024-01-19 10:03:12 -0300

    fetch android launcher releases

--- build-install.js
+++ build-install.js
@@ -3,10 +3,13 @@ import { readFileSync, writeFileSync } from 'fs';
 let installPage = readFileSync('src/install/index.html').toString();
 
 console.log("Fetching latest Geode release from github");
-fetch('https://api.github.com/repos/geode-sdk/geode/releases/latest')
-    .then(r => r.json())
-    .then(data => {
-        const str = JSON.stringify(data);
-        installPage = installPage.replace("{$GITHUB_RELEASE_DATA:0}", str);
-        writeFileSync("gen/install/index.html", installPage);
-    });
+async function main() {
+    const data = await (await fetch('https://api.github.com/repos/geode-sdk/geode/releases/latest')).json();
+    installPage = installPage.replace("{$GITHUB_RELEASE_DATA:0}", JSON.stringify(data));
+
+    const dataLauncher = await (await fetch('https://api.github.com/repos/geode-sdk/android-launcher/releases/latest')).json();
+    installPage = installPage.replace("{$GITHUB_ANDROID_LAUNCHER_RELEASE_DATA:0}", JSON.stringify(dataLauncher));
+
+    writeFileSync("gen/install/index.html", installPage);
+}
+main();
