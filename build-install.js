67ecabf7a95aaa4a54cc805db3c063c7decb6224
matcool <26722564+matcool@users.noreply.github.com>
2024-02-14 12:20:13 -0300

    remove user js code to generate install page

--- build-install.js
+++ build-install.js
@@ -1,14 +1,51 @@
 import { readFileSync, writeFileSync } from 'fs';
+import { htmlEscape } from 'escape-goat';
+import * as feather from 'feather-icons';
 
 let installPage = readFileSync('src/install/index.html').toString();
 
 console.log("Fetching latest Geode release from github");
 async function main() {
     const data = await (await fetch('https://api.github.com/repos/geode-sdk/geode/releases/latest')).json();
-    installPage = installPage.replace("{$GITHUB_RELEASE_DATA:0}", JSON.stringify(data));
-
+    console.log(`Latest version is ${data.tag_name}`);
     const dataLauncher = await (await fetch('https://api.github.com/repos/geode-sdk/android-launcher/releases/latest')).json();
-    installPage = installPage.replace("{$GITHUB_ANDROID_LAUNCHER_RELEASE_DATA:0}", JSON.stringify(dataLauncher));
+    console.log(`Latest launcher version is ${dataLauncher.tag_name}`);
+
+
+    installPage = installPage.replace("$LATEST_VERSION_TAG", data.tag_name);
+
+    let platformsHtml = "";
+    const addPlatform = (name, url) => {
+        platformsHtml += `<a href="${url}" class="button wide border-solid border-2 border-gray-light text-left" onclick="scrollToTutorial()">
+            ${feather.icons.box.toSvg()} Download for ${htmlEscape(name)}
+        </a>`;
+    };
+
+    dataLauncher.assets.reverse();
+    for (const asset of [...data.assets, ...dataLauncher.assets]) {
+        let assetName = asset.name.toLowerCase();
+        let platformName;
+
+        if (assetName.includes('installer')) {
+            if (assetName.includes('mac')) {
+                platformName = 'Mac';
+            } else if (assetName.includes('win')) {
+                platformName = 'Windows';
+            }
+        } else if (assetName.includes('.apk')) {
+            if (assetName.endsWith('-android32.apk')) {
+                platformName = 'Android (32 bit)';
+            } else {
+                platformName = 'Android';
+            }
+        }
+        if (!platformName) continue;
+
+        addPlatform(platformName, asset.browser_download_url);
+    }
+
+    installPage = installPage.replace("$INSTALL_OPTIONS_HTML", platformsHtml);
+
 
     writeFileSync("gen/install/index.html", installPage);
 }
