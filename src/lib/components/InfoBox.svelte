a9df15dbd69b4e950f5b23f33a329b2c5f347d8e
HJfod <60038575+HJfod@users.noreply.github.com>
2024-06-09 01:13:45 +0300

    infobox component

---
+++ src/lib/components/InfoBox.svelte
@@ -0,0 +1,46 @@
+
+<script lang="ts">
+    import Icon from "./Icon.svelte";
+
+    export let type: 'info' | 'warning';
+</script>
+
+<section class={type}>
+    <span class="icon-container">
+        <Icon icon={type}/>
+    </span>
+    <span class="content-container"><slot/></span>
+</section>
+
+<style lang="scss">
+    section {
+        display: flex;
+        flex-direction: row;
+        border-radius: .5rem;
+        max-width: min(60ch, 90vw);
+
+        .icon-container {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            min-width: 3em;
+        }
+        .content-container {
+            padding: .5rem;
+        }
+        &.info {
+            background-color: color-mix(in srgb, var(--accent-alt-500) 35%, transparent);
+            border: .2rem solid color-mix(in srgb, var(--accent-alt-500) 35%, transparent);
+            .icon-container {
+                color: var(--accent-alt-300);
+            }
+        }
+        &.warning {
+            background-color: color-mix(in srgb, var(--primary-300) 25%, transparent);
+            border: .2rem solid color-mix(in srgb, var(--primary-300) 25%, transparent);
+            .icon-container {
+                color: var(--accent-300);
+            }
+        }
+    }
+</style>
