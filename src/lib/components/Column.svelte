7c312ee8feff7c798cfef446b45d7a22ba4d835b
HJfod <60038575+HJfod@users.noreply.github.com>
2024-06-09 01:13:07 +0300

    allow reversing column

--- src/lib/components/Column.svelte
+++ src/lib/components/Column.svelte
@@ -8,9 +8,10 @@
     export let align: keyof typeof alignment = 'center';
     export let gap: Gap = 'normal';
     export let wrap: boolean = false;
+    export let reverse: boolean = false;
 </script>
 
-<div style="--wrap: {wrap ? 'wrap' : 'nowrap'}; --gap: var(--gap-{gap}); --align: {alignment[align]}"><slot/></div>
+<div style="--wrap: {wrap ? 'wrap' : 'nowrap'}; --gap: var(--gap-{gap}); --align: {alignment[align]}" class:reverse><slot/></div>
 
 <style lang="scss">
     div {
@@ -19,5 +20,8 @@
         flex-wrap: var(--wrap);
         gap: var(--gap);
         align-items: var(--align);
+        &.reverse {
+            flex-direction: column-reverse;
+        }
     }
 </style>
