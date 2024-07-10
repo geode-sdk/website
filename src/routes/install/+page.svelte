47d28ede387c4285461b90533c6d8b316e74104b
Cvolton <cvolton@cvolton.eu>
2024-07-10 10:09:52 +0200

    bump göde

--- src/routes/install/+page.svelte
+++ src/routes/install/+page.svelte
@@ -13,7 +13,7 @@
     import { onMount } from "svelte";
 
     // Until server returns this, we're doing it manually
-    let latestVersion = "v3.1.1";
+    let latestVersion = "v3.2.0";
     let latestLauncher = "v1.4.1";
     let showAllPlatforms = false;
     let curPlatform: 'windows' | 'mac' | 'android' | 'linux' | 'unknown' | undefined = undefined;
