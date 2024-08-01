05d96ce08c1faa3571fa975b4262808a9cd6daad
Chloe <25387744+qimiko@users.noreply.github.com>
2024-07-31 22:19:54 -0700

    put filters into a separate component

--- src/routes/mods/+page.svelte
+++ src/routes/mods/+page.svelte
@@ -7,18 +7,16 @@
     import Gap from "$lib/components/Gap.svelte";
     import Column from "$lib/components/Column.svelte";
     import SelectButton from "$lib/components/SelectButton.svelte";
-    import Icon from "$lib/components/Icon.svelte";
-    import { iconForTag } from "$lib/index.js";
     import Row from "$lib/components/Row.svelte";
     import Select from "$lib/components/Select.svelte";
     import Search from "$lib/components/Search.svelte";
     import SelectOption from "$lib/components/SelectOption.svelte";
-    import Rollover from "$lib/components/Rollover.svelte";
     import Waves from "$lib/components/Waves.svelte";
     import Button from "$lib/components/Button.svelte";
     import LoadingCircle from "$lib/components/LoadingCircle.svelte";
     import Image from "$lib/components/Image.svelte";
     import InfoBox from "$lib/components/InfoBox.svelte";
+    import FilterMenu from "$lib/components/FilterMenu.svelte";
 
     export let data: PageData;
 
@@ -43,10 +41,6 @@
     $: max_page = Math.floor(max_count / per_page) + 1;
 
     const perPageOptions = [10, 15, 25];
-
-    function toggleSet<T>(set: Set<T>, value: T) {
-        set.has(value) ? set.delete(value) : set.add(value);
-    }
 
     const updateQuery = () => {
         // debouce search bar so we're not making a ton of useless requests
@@ -134,88 +128,13 @@
 <h1>Browse Mods</h1>
 
 <div class="content-separator">
-    <aside>
-        <header><Icon icon="filter" --icon-size=1.2em/>Search Filters</header>
-        <nav>
-            <Rollover title="Platform">
-                <SelectButton
-                    icon="windows"
-                    selected={platforms.has('windows')}
-                    on:select={() => {
-                        toggleSet(platforms, 'windows');
-                        updateSearch();
-                    }}
-                >Windows</SelectButton>
-                <SelectButton
-                    icon="mac"
-                    selected={platforms.has('mac-arm')}
-                    on:select={() => {
-                        toggleSet(platforms, 'mac-arm');
-                        updateSearch();
-                    }}
-                >macOS (ARM)</SelectButton>
-                <SelectButton
-                    icon="mac"
-                    selected={platforms.has('mac-intel')}
-                    on:select={() => {
-                        toggleSet(platforms, 'mac-intel');
-                        updateSearch();
-                    }}
-                >macOS (x64)</SelectButton>
-                <SelectButton
-                    icon="android"
-                    selected={platforms.has('android64')}
-                    on:select={() => {
-                        toggleSet(platforms, 'android64');
-                        updateSearch();
-                    }}
-                >Android (64-bit)</SelectButton>
-                <SelectButton
-                    icon="android"
-                    selected={platforms.has('android32')}
-                    on:select={() => {
-                        toggleSet(platforms, 'android32');
-                        updateSearch();
-                    }}
-                >Android (32-bit)</SelectButton>
-            </Rollover>
-
-            <Rollover title="Tags">
-                {#await data.tags}
-                    <LoadingCircle size="small" />
-                {:then server_tags}
-                    {#if server_tags}
-                        {#each server_tags as tag}
-                            <SelectButton
-                                icon={iconForTag(tag)}
-                                selected={tags.has(tag)}
-                                on:select={() => {
-                                    toggleSet(tags, tag);
-                                    updateSearch();
-                                }}>
-                                {tag.charAt(0).toUpperCase() + tag.slice(1)}
-                            </SelectButton>
-                        {/each}
-                    {:else}
-                        <InfoBox type="error">Failed to list tags!</InfoBox>
-                    {/if}
-                {:catch error}
-                    <InfoBox type="error">Unable to connect to servers!</InfoBox>
-                {/await}
-            </Rollover>
-
-            <Rollover title="Other">
-                <SelectButton
-                    icon="featured"
-                    bind:selected={featured} on:select={updateSearch}
-                >Featured only</SelectButton>
-                <SelectButton
-                    icon="unverified"
-                    bind:selected={pending} on:select={updateSearch}
-                >Unverified only</SelectButton>
-            </Rollover>
-        </nav>
-    </aside>
+    <FilterMenu
+        platforms={platforms}
+        tags={tags}
+        tagsListing={data.tags}
+        featured={featured}
+        pending={pending}
+        on:update={updateSearch} />
 
     <Column align="stretch" gap="small">
         <nav class="search">
@@ -366,30 +285,6 @@
         align-items: start;
         gap: var(--gap-small);
 
-        & > aside {
-            background-color: var(--background-950);
-            padding: .5rem;
-            gap: .5rem;
-            border-radius: .5rem;
-
-            display: flex;
-            flex-direction: column;
-            align-items: stretch;
-
-            & > header {
-                display: flex;
-                flex-direction: row;
-                align-items: center;
-                color: var(--text-300);
-                font-size: .9em;
-                gap: var(--gap-small);
-            }
-            & > nav {
-                display: flex;
-                flex-direction: column;
-                gap: var(--gap-small);
-            }
-        }
         & main {
             display: grid;
             grid-template-columns: 1fr;
