b02d48574f01dcc34b8aac7026cd197f665297bb
Chloe <25387744+qimiko@users.noreply.github.com>
2024-08-04 16:28:43 -0700

    use redis for slightly more proper caching
    commit 0e8d5ddf7a677fbd7f12388c38ab92db3132fa3d
    Author: Chloe <25387744+qimiko@users.noreply.github.com>
    Date:   Sun Aug 4 03:39:12 2024 -0700
    
        handles your errors
    
    commit 1b39bbf5544cb3cd3824cb711837f0b9093c8fb4
    Author: Chloe <25387744+qimiko@users.noreply.github.com>
    Date:   Sun Aug 4 02:13:01 2024 -0700
    
        silly idea
    
    commit 60b69048340fa147fa987faf579edde3e6ab5701
    Author: Chloe <25387744+qimiko@users.noreply.github.com>
    Date:   Sun Aug 4 01:58:36 2024 -0700
    
        add lock during fetch
    
    commit 3040b90654c13e60e58427211fd1a46f18c09310
    Author: Chloe <25387744+qimiko@users.noreply.github.com>
    Date:   Sat Aug 3 17:23:09 2024 -0700
    
        initial redis implementation

---
+++ src/lib/server/redis.ts
@@ -0,0 +1,73 @@
+import * as privateEnv from "$env/static/private";
+import { createClient } from "redis";
+
+const redisUrl =
+    "PRIVATE_REDIS_URL" in privateEnv &&
+    typeof privateEnv.PRIVATE_REDIS_URL == "string"
+        ? privateEnv.PRIVATE_REDIS_URL
+        : null;
+
+export const redis = redisUrl ? createClient({ url: redisUrl }) : null;
+
+function formatError(e: Error) {
+    return `${e.name}: ${e.message}`;
+}
+
+redis?.on("error", (e: Error) => {
+    if (e instanceof AggregateError) {
+        console.error(
+            `Redis threw errors: ${e.errors.map((a) => formatError(a))}`,
+        );
+    } else {
+        console.error(`Redis threw error: ${formatError(e)}`);
+    }
+});
+
+await redis?.connect();
+
+async function asyncTimeout(timeoutMs: number) {
+    return new Promise((r) => setTimeout(r, timeoutMs));
+}
+
+/**
+ * waits for exclusive access to the database based on the given id. no effect is redis is not present
+ * @param id cache key to use for lock
+ * @param timeoutMs amount of time (ms) to wait before stopping and throwing an error
+ */
+export async function redis_lock(id: string, timeoutMs = 5_000) {
+    if (!redis) {
+        return;
+    }
+
+    const initialWaitMs = 10;
+
+    let timeWaited = 0;
+    let iterations = 1;
+
+    while (timeWaited < timeoutMs) {
+        const waitPeriod = iterations * initialWaitMs;
+
+        if (await redis.set(id, "lock", { NX: true, EX: timeoutMs / 1_000 })) {
+            return;
+        }
+
+        await asyncTimeout(waitPeriod);
+
+        timeWaited += waitPeriod;
+        iterations++;
+    }
+
+    throw new Error(`failed to acquire lock: ${id}`);
+}
+
+/**
+ * unlocks the exclusive access given in redis_lock. no effect if redis is not present
+ * @param id cache key used in redis_lock
+ */
+export async function redis_unlock(id: string) {
+    if (!redis) {
+        return;
+    }
+
+    await redis.del(id);
+}
