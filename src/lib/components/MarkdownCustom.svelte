d021bc3e01c80f98b8533cc2825ab7b6fa6c3e07
Chloe <25387744+qimiko@users.noreply.github.com>
2024-12-12 19:39:32 -0700

    colors!

---
+++ src/lib/components/MarkdownCustom.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let text: string | undefined = undefined;
+	export let color: string | undefined = undefined;
+	export let raw: string;
+</script>
+
+{#if text}
+	<span style={`color: ${color}`}>{raw}</span>
+{:else}
+	<slot />
+{/if}
