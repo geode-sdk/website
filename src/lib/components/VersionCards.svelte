2cd2aec84f9d1bdcdef723101b0b31f05ae480a7
Chloe <25387744+qimiko@users.noreply.github.com>
2025-11-05 16:43:52 -0700

    merge versions if available on all platforms

--- src/lib/components/VersionCards.svelte
+++ src/lib/components/VersionCards.svelte
@@ -9,6 +9,11 @@
     }
 
     let { gd, longForm = false }: Props = $props();
+
+    const canMerge = $derived(
+        !longForm &&
+            [gd.android32, gd.android64, gd.ios, gd["mac-arm"], gd["mac-intel"], gd.win].every((x) => x == gd.ios),
+    );
 </script>
 
 <Icon icon="gd" />
@@ -17,30 +22,34 @@
     Available on:
 {/if}
 
-{#if gd.win}<Label icon="windows" design="gray">{gd.win}</Label>{/if}
-
-{#if gd["mac-arm"] && gd["mac-arm"] === gd["mac-intel"]}
-    <Label icon="mac" design="gray">{gd["mac-arm"]}</Label>
+{#if canMerge}
+    {gd.ios}
 {:else}
-    {#if gd["mac-arm"]}
-        <Label icon="mac" design="gray">{gd["mac-arm"]} (ARM)</Label>
-    {/if}
-    {#if gd["mac-intel"]}
-        <Label icon="mac" design="gray">{gd["mac-intel"]} (x64)</Label>
-    {/if}
-{/if}
+    {#if gd.win}<Label icon="windows" design="gray">{gd.win}</Label>{/if}
 
-{#if gd.ios}
-    <Label icon="ios" design="gray">{gd.ios}</Label>
-{/if}
-
-{#if gd.android64 && gd.android64 === gd.android32}
-    <Label icon="android" design="gray">{gd.android64}</Label>
-{:else}
-    {#if gd.android64}
-        <Label icon="android" design="gray">{gd.android64} (64-bit)</Label>
+    {#if gd["mac-arm"] && gd["mac-arm"] === gd["mac-intel"]}
+        <Label icon="mac" design="gray">{gd["mac-arm"]}</Label>
+    {:else}
+        {#if gd["mac-arm"]}
+            <Label icon="mac" design="gray">{gd["mac-arm"]} (ARM)</Label>
+        {/if}
+        {#if gd["mac-intel"]}
+            <Label icon="mac" design="gray">{gd["mac-intel"]} (x64)</Label>
+        {/if}
     {/if}
-    {#if gd.android32}
-        <Label icon="android" design="gray">{gd.android32} (32-bit)</Label>
+
+    {#if gd.ios}
+        <Label icon="ios" design="gray">{gd.ios}</Label>
+    {/if}
+
+    {#if gd.android64 && gd.android64 === gd.android32}
+        <Label icon="android" design="gray">{gd.android64}</Label>
+    {:else}
+        {#if gd.android64}
+            <Label icon="android" design="gray">{gd.android64} (64-bit)</Label>
+        {/if}
+        {#if gd.android32}
+            <Label icon="android" design="gray">{gd.android32} (32-bit)</Label>
+        {/if}
     {/if}
 {/if}
