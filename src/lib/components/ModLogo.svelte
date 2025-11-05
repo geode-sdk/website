7b1f8b7fdf6220693e662c47b084b7939060fd05
Chloe <25387744+qimiko@users.noreply.github.com>
2025-11-04 23:13:17 -0700

    update placeholder code

--- src/lib/components/ModLogo.svelte
+++ src/lib/components/ModLogo.svelte
@@ -1,5 +1,3 @@
-<!-- @migration-task Error while migrating Svelte code: Event attribute must be a JavaScript expression, not a string
-https://svelte.dev/e/attribute_invalid_event_handler -->
 <script lang="ts">
     import { IndexClient } from "$lib/api/index-repository.js";
     import type { ServerMod } from "$lib/api/models/mod.js";
@@ -8,22 +6,28 @@ https://svelte.dev/e/attribute_invalid_event_handler -->
     import iconPlaceholder from "$lib/assets/icon-placeholder.png";
     import iconPending from "$lib/assets/icon-pending.png";
 
-    export let mod: ServerMod;
-    export let version: ServerModVersion;
-    export let size: "large" | "medium" | "small" = "small";
+    interface Props {
+        mod: ServerMod;
+        version: ServerModVersion;
+        size: "large" | "medium" | "small";
+    }
 
-    $: iconSize = size == "large" ? "8rem" : size == "medium" ? "6rem" : "5rem";
+    const { mod, version, size = "small" } = $props();
 
-    $: accepted = version.status == "accepted";
+    const iconSize = $derived(size == "large" ? "8rem" : size == "medium" ? "6rem" : "5rem");
+
+    const accepted = $derived(version.status == "accepted");
 
     // put both the version and status into the cache to prevent stale icons from being cached
     // (pending icons show the old accepted icon for that version until that version is accepted)
-    $: logoUrl = IndexClient.getModLogo(mod.id, {
-        version: version.version,
-        status: version.status != "accepted" ? version.status : undefined,
-    }).toString();
+    let logoUrl = $derived(
+        IndexClient.getModLogo(mod.id, {
+            version: version.version,
+            status: version.status != "accepted" ? version.status : undefined,
+        }).toString(),
+    );
 
-    $: placeholderIcon = accepted ? iconPlaceholder : iconPending;
+    const placeholderIcon = $derived(accepted ? iconPlaceholder : iconPending);
 
     const checkPlaceholder = (node: HTMLImageElement) => {
         if (node.complete) {
@@ -34,7 +38,7 @@ https://svelte.dev/e/attribute_invalid_event_handler -->
         }
     };
 
-    let imageLoaded = false;
+    let imageLoaded = $state(false);
 </script>
 
 <!-- on:error doesn't run early enough, but the language server complains, lol -->
