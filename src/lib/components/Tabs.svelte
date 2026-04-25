6583594cc421ef0c67d68c08f53186b38a43c8cb
Fleeym <fleeym@proton.me>
2026-04-25 18:18:51 +0300

    feat: use Buttons for Tab headings

--- src/lib/components/Tabs.svelte
+++ src/lib/components/Tabs.svelte
@@ -12,6 +12,7 @@
     import { writable, type Writable } from "svelte/store";
     import cx from "$lib/cx";
     import Icon from "./Icon.svelte";
+    import Button from "./Button.svelte";
     interface Props {
         children?: Snippet;
     }
@@ -30,16 +31,13 @@
 <div class="flex flex-col items-start gap-3">
     <div class="flex flex-wrap gap-2">
         {#each $tabs as tab}
-            <button
-                class={cx(
-                    "flex items-center gap-1 rounded-md border-2 border-solid bg-transparent p-2 py-1 text-sm text-zinc-300 transition",
-                    "hover:cursor-pointer hover:bg-(--background-600)",
-                    $selectedTab === tab.id && "bg-(--background-500) text-zinc-50",
-                )}
+            <Button
                 onclick={() => ($selectedTab = tab.id)}
-                class:selected={$selectedTab === tab.id}>
+                type="button"
+                size="small"
+                design={$selectedTab === tab.id ? "secondary-filled" : "hollow"}>
                 <Icon icon={tab.icon} />{tab.name}
-            </button>
+            </Button>
         {/each}
     </div>
     <div class="pages">{@render children?.()}</div>
