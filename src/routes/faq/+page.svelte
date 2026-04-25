8070f0ad2f9549731365df7e66e67b204fa33286
Fleeym <fleeym@proton.me>
2026-04-25 19:10:32 +0300

    set faq nav overflow to auto

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -31,7 +31,7 @@
 <h1 class="font-heading mt-30 text-5xl font-bold">Frequently Asked Questions</h1>
 
 <div class="flex items-start justify-center gap-2">
-    <div class="sticky top-20 max-h-[65vw] overflow-y-scroll">
+    <div class="sticky top-20 max-h-[65vw] overflow-y-auto">
         <QuestionNav {faqs} />
     </div>
     <div class="w-full">
