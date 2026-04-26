353f148035a7fda64591968c8a03b4c0506d8a88
Fleeym <fleeym@proton.me>
2026-04-26 12:53:25 +0300

    switch to textarea component

--- src/lib/components/Threads/ModThreadCommentForm.svelte
+++ src/lib/components/Threads/ModThreadCommentForm.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
+    import { enhance } from "$app/forms";
     import Button from "../Button.svelte";
     import Icon from "../Icon.svelte";
+    import Textarea from "../ui/Textarea.svelte";
 </script>
 
 <div class="w-full">
-    <form id="mod-thread-new-comment-form" action="" class="flex flex-col">
+    <form id="mod-thread-new-comment-form" method="POST" action="?/comment" class="flex flex-col" use:enhance>
         <div class="max-h-60">
-            <textarea
+            <Textarea
                 name="comment"
                 id="mod-thread-comment"
-                class="border-background-800 field-sizing-content h-full max-h-60 min-h-30 w-full overflow-auto rounded rounded-b-none border-2 p-2 outline-0"
-                placeholder="Enter your new comment here"></textarea>
+                placeholder="Enter your new comment here"
+                class="h-full max-h-60 min-h-30" />
         </div>
         <div class="bg-background-800 flex justify-between gap-2 rounded rounded-t-none p-2">
             <small>Press Enter to post. Press Shift + Enter to go on a new row.</small>
